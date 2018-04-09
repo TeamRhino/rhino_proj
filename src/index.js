2deb939ceccfbbe58a9ba3987b793f40a73ee060
chokevin <cap.kevincho@gmail.com>
2018-04-08 22:49:32 -1000

    merged in changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,9 @@ import './index.css';
 import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
 import { history } from './helpers';
 import {
     Router
-=======
-import {
-    BrowserRouter
->>>>>>> master
   } from 'react-router-dom'
 import { store } from './helpers';
 import 'semantic-ui-css/semantic.min.css';
@@ -22,18 +17,10 @@ import { configureFakeBackend } from './helpers';
 configureFakeBackend();
 
 ReactDOM.render(
-<<<<<<< HEAD
         <Router history={history}>
             <Provider store={store}>
                 <App />
             </Provider>
         </Router>,
-=======
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>,
->>>>>>> master
         document.getElementById('root'));
 registerServiceWorker();
